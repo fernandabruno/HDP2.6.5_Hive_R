de6c1721a701f81b7d42c5593321e7f639d55a86
Fernanda Bruno <februno@gmail.com>
2020-10-05 20:11:44 -0300

    Update HiveHDP_JDBC.R
    Adjusting jar dependencies.

--- HiveHDP_JDBC.R
+++ HiveHDP_JDBC.R
@@ -1,10 +1,10 @@
 #########################
 ###    Hive + RJDBC   ###
 ### HDP 2.6.5 Sandbox ###
-### September 13,2020 ###
+### October 5,2020 ###
 #########################
 
-#loading required libraries
+#Loading required libraries
 library("DBI")
 library("rJava")
 library("RJDBC")
@@ -12,9 +12,9 @@ library("RJDBC")
 options( java.parameters = "-Xmx8g" )
 
 #init of the classpath (tested with HD 2.6.5 Sandbox)
-cp = c("/usr/hdp/2.6.5.0-292/hadoop/hadoop-common.jar","/usr/hdp/2.6.5.0-292/hadoop/hadoop-auth.jar", 
-       "/usr/hdp/2.6.5.0-292/hive/jdbc/hive-jdbc-1.2.1000.2.6.5.0-292-standalone.jar",
-       "/usr/hdp/2.6.5.0-292/hive2/lib/hive-service.jar")
+cp = c("/usr/hdp/2.6.5.0-292/hadoop/hadoop-common.jar",
+       "/usr/hdp/2.6.5.0-292/hadoop/hadoop-auth.jar", 
+       "/usr/hdp/2.6.5.0-292/hive/jdbc/hive-jdbc-1.2.1000.2.6.5.0-292-standalone.jar")
 .jinit(classpath=cp)
 
 #Creating JDBC connection
