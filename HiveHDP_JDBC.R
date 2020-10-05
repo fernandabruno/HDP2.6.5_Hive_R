3e2005ee4165ef3cff4d83ca8984ee51e023e5ed
Fernanda Bruno <februno@gmail.com>
2020-10-05 20:07:00 -0300

    Update HiveHDP_JDBC.R
    Adjusting jars

--- HiveHDP_JDBC.R
+++ HiveHDP_JDBC.R
@@ -4,25 +4,29 @@
 ### September 13,2020 ###
 #########################
 
-#Loading required libraries
+#loading required libraries
 library("DBI")
 library("rJava")
 library("RJDBC")
 
+options( java.parameters = "-Xmx8g" )
+
 #init of the classpath (tested with HD 2.6.5 Sandbox)
-cp = c("/usr/hdp/2.6.5.0-292/hadoop/hadoop-common.jar", "/usr/hdp/2.6.5.0-292/hive2/jdbc/hive-jdbc-2.1.0.2.6.5.0-292-standalone.jar")
+cp = c("/usr/hdp/2.6.5.0-292/hadoop/hadoop-common.jar","/usr/hdp/2.6.5.0-292/hadoop/hadoop-auth.jar", 
+       "/usr/hdp/2.6.5.0-292/hive/jdbc/hive-jdbc-1.2.1000.2.6.5.0-292-standalone.jar",
+       "/usr/hdp/2.6.5.0-292/hive2/lib/hive-service.jar")
 .jinit(classpath=cp)
 
 #Creating JDBC connection
-drv <- JDBC("org.apache.hive.jdbc.HiveDriver", "/usr/hdp/2.6.5.0-292/hive2/jdbc/hive-jdbc-2.1.0.2.6.5.0-292-standalone.jar", identifier.quote="`")
+drv <- JDBC("org.apache.hive.jdbc.HiveDriver", "/usr/hdp/2.6.5.0-292/hive/jdbc/hive-jdbc-1.2.1000.2.6.5.0-292-standalone.jar", identifier.quote="`")
 
 #Creating JDBC connection
-conn <- dbConnect(drv, "jdbc:hive2://localhost:10000/", "user", "password")
+conn <- dbConnect(drv, "jdbc:hive2://localhost:10000/",user="user",password="password")
 
 #Checking connection
 result <- dbGetQuery(conn, "show databases")
 result
 
 #Verifying tables available in selected database
-tables <- dbGetQuery(conn, "show tables in database")
+tables <- dbGetQuery(conn, "show tables in your_database")
 tables
